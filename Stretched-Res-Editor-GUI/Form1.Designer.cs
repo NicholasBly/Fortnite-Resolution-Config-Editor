bde9d7458ac46ba624aedd9f58adae1f76f3c028
Nicholas Bly <73457207+NicholasBly@users.noreply.github.com>
2023-02-06 04:06:36 -0500

    Add files via upload

---
+++ Stretched-Res-Editor-GUI/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace Stretched_Res_Editor_GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.filePath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Button1 = new System.Windows.Forms.Button();
+            this.newResX = new System.Windows.Forms.TextBox();
+            this.newResY = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.curResY = new System.Windows.Forms.TextBox();
+            this.curResX = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button3 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // filePath
+            // 
+            this.filePath.Location = new System.Drawing.Point(154, 24);
+            this.filePath.Name = "filePath";
+            this.filePath.Size = new System.Drawing.Size(277, 20);
+            this.filePath.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "GameUserSettings.ini Path:";
+            // 
+            // Button1
+            // 
+            this.Button1.Location = new System.Drawing.Point(437, 24);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(75, 20);
+            this.Button1.TabIndex = 2;
+            this.Button1.Text = "Set Path";
+            this.Button1.UseVisualStyleBackColor = true;
+            this.Button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // newResX
+            // 
+            this.newResX.Location = new System.Drawing.Point(268, 124);
+            this.newResX.Name = "newResX";
+            this.newResX.Size = new System.Drawing.Size(100, 20);
+            this.newResX.TabIndex = 3;
+            // 
+            // newResY
+            // 
+            this.newResY.Location = new System.Drawing.Point(268, 150);
+            this.newResY.Name = "newResY";
+            this.newResY.Size = new System.Drawing.Size(100, 20);
+            this.newResY.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(155, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "New Resolution X:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(155, 153);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "New Resolution Y:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(158, 176);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(210, 20);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Set Resolution";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(155, 101);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(107, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Current Resolution Y:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(155, 75);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(107, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Current Resolution X:";
+            // 
+            // curResY
+            // 
+            this.curResY.Location = new System.Drawing.Point(268, 98);
+            this.curResY.Name = "curResY";
+            this.curResY.Size = new System.Drawing.Size(100, 20);
+            this.curResY.TabIndex = 9;
+            // 
+            // curResX
+            // 
+            this.curResX.Location = new System.Drawing.Point(268, 72);
+            this.curResX.Name = "curResX";
+            this.curResX.Size = new System.Drawing.Size(100, 20);
+            this.curResX.TabIndex = 8;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(555, 24);
+            this.menuStrip1.TabIndex = 12;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(93, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(518, 24);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(27, 20);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "?";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(555, 202);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.curResY);
+            this.Controls.Add(this.curResX);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.newResY);
+            this.Controls.Add(this.newResX);
+            this.Controls.Add(this.Button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.filePath);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Fortnite Resolution Changer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox filePath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Button1;
+        private System.Windows.Forms.TextBox newResX;
+        private System.Windows.Forms.TextBox newResY;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox curResY;
+        private System.Windows.Forms.TextBox curResX;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Stretched-Res-Editor-GUI/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Stretched_Res_Editor_GUI
+{
+    public partial class Form1 : Form
+    {
+        const string pathLoc = "path.txt";
+        string GameUserSettingsiniPath;
+        string resx1 = "ResolutionSizeX=";
+        string resx2 = "LastUserConfirmedResolutionSizeX=";
+        string resx3 = "DesiredScreenWidth=";
+        string resx4 = "LastUserConfirmedDesiredScreenWidth=";
+        string resy1 = "ResolutionSizeY=";
+        string resy2 = "LastUserConfirmedResolutionSizeY=";
+        string resy3 = "DesiredScreenHeight=";
+        string resy4 = "LastUserConfirmedDesiredScreenHeight=";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void getResXandY()
+        {
+            try
+            {
+                if (GameUserSettingsiniPath != null)
+                {
+                    string n = File.ReadAllText(GameUserSettingsiniPath);
+                    var result = n.Split('\n').Where(x => x.Contains(resx1)).Select(x => x.Replace(resx1, string.Empty)).FirstOrDefault().Trim();
+                    string currentResolutionX = result.Substring(result.Length - 4);
+                    curResX.Text = currentResolutionX;
+
+                    var result2 = n.Split('\n').Where(x => x.Contains(resy1)).Select(x => x.Replace(resy1, string.Empty)).FirstOrDefault().Trim();
+                    string currentResolutionY = result2.Substring(result2.Length - 4);
+                    curResY.Text = currentResolutionY;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("There was an error reading your path, please make sure it is in the proper format.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists(pathLoc) && new FileInfo(pathLoc).Length != 0)
+            {
+                const string f = pathLoc;
+                List<string> lines = new List<string>();
+                using (StreamReader r = new StreamReader(f))
+                {
+                    string line;
+                    while ((line = r.ReadLine()) != null)
+                    {
+                        lines.Add(line);
+                    }
+                    r.Close();
+                    r.Dispose();
+                }
+                filePath.Text = lines[0];
+                GameUserSettingsiniPath = filePath.Text;
+
+                getResXandY();
+            }
+            else
+            {
+                File.Create(pathLoc).Close();
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            using (StreamWriter objWriter = new StreamWriter(pathLoc))
+            {
+                objWriter.Write(filePath.Text);
+                objWriter.Dispose();
+                objWriter.Close();
+            }
+            MessageBox.Show("Path Set. Please restart to get your current X and Y values added.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists(pathLoc))
+            {
+                MessageBox.Show("You have not selected a path to the GameUserSettings.ini file!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+            }
+            else
+            {
+                if (!String.IsNullOrEmpty(curResX.Text) && !String.IsNullOrEmpty(curResY.Text) && GameUserSettingsiniPath != null)
+                {
+                    string text = File.ReadAllText(GameUserSettingsiniPath);
+                    text = text.Replace(resx1 + curResX.Text, resx1 + newResX.Text); // replace
+                    text = text.Replace(resx2 + curResX.Text, resx2 + newResX.Text); // replace
+                    text = text.Replace(resx3 + curResX.Text, resx3 + newResX.Text); // replace
+                    text = text.Replace(resx4 + curResX.Text, resx4 + newResX.Text); // replace
+
+                    text = text.Replace(resy1 + curResY.Text, resy1 + newResY.Text); // replace
+                    text = text.Replace(resy2 + curResY.Text, resy2 + newResY.Text); // replace
+                    text = text.Replace(resy3 + curResY.Text, resy3 + newResY.Text); // replace
+                    text = text.Replace(resy4 + curResY.Text, resy4 + newResY.Text); // replace
+                    File.WriteAllText(GameUserSettingsiniPath, text);
+                    curResX.Text = newResX.Text;
+                    curResY.Text = newResY.Text;
+                    MessageBox.Show("Resolution Set!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Please enter your current X and Y resolutions to continue, and/or make sure your path is set correctly", "Error", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            getResXandY();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.GetCurrentProcess().Kill();
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("To find your GameUserSettings.ini file path:\n\nPress Windows Key + R to bring up Run.\nType in %localappdata% into the search bar and hit Enter.\nFind the FortniteGame folder and click on it.\nClick on the Saved folder.\nClick on the Config folder.\nClick on the WindowsClient folder.\nCopy the path by clicking the address bar at the top of Windows Explorer, and copying the path.\nMake sure you include " + "\\" + "GameUserSettings.ini at the end of the path.\n\nAn example would look like this:\n\nC:\\Users\\YOURUSERNAME\\AppData\\Local\\FortniteGame\\Saved\\Config\\WindowsClient\\GameUserSettings.ini");
+        }
+    }
+}
